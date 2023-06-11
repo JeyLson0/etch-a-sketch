453effed6078a78da2211cdcb8bc6e0c162cba48
Jeylson0 <jhonalson.lugada@gmail.com>
2023-06-12 03:42:36 +0800

    Version 1.0.1 Feat: Range Value
    Add range scroll that sets how many pixels inside the grid.

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-let 
-const gridBox = document.querySelector('#grid')
+//Variables
+let rangeBar = document.querySelector('#range_bar');
+let slider = document.querySelector('#grid_slider');
+let rangeValue = document.querySelector('#range_value');
+let gridValue = slider.value;
+let gridBox = document.querySelector('#grid');
+let gridList = gridBox.querySelectorAll('div')
+let gridBoxNum = gridBox.getElementsByClassName('pixel')
+let pixelNum = gridBoxNum.length
+
+insertDivs();
+function insertDivs() {
+    let totalGridDivs = gridValue * gridValue;
+    for (i=0; i<totalGridDivs; i++) {
+        let gridDivs = document.createElement('div');
+        gridDivs.classList.add('pixel')
+        gridBox.appendChild(gridDivs);
+    }
+    pixelNum = gridBoxNum.length;
+    console.log(pixelNum)
+
+    if (totalGridDivs != pixelNum) {
+        let excessDivs = pixelNum - totalGridDivs;
+        for(i=0; i<excessDivs; i++) {
+            gridBox.removeChild(gridBox.firstElementChild);
+        }
+    }
+    pixelNum = gridBoxNum.length;
+    console.log(pixelNum)
+}
+
+//Range bar
+rangeValue.textContent= `${slider.value} x ${slider.value}`;
+slider.oninput = function Range() {
+    rangeValue.textContent = `${slider.value} x ${slider.value}`;
+    gridValue = slider.value;
+    insertDivs();
+}
+
+
+
+
+
+/* gridRange();
+function gridRange() {
+
+} */
+
+//GRID and grid size
+
+
 
 //function will fill divContainer 
-function fillContainer () {
-    
-}
